495dd32fbacd4758ea38b1fccdecd56a8b30b802
Stuart Pook <stuart@dataseat.com>
2025-11-27 22:27:40 +0100

    add brightness and transitions

--- busylight-hass.py
+++ busylight-hass.py
@@ -51,6 +51,7 @@ def get_options():
     parser.add_argument("--red", type=int, metavar="INTEGER", default=255, help="red level")
     parser.add_argument("--green", type=int, metavar="INTEGER", default=0, help="green level")
     parser.add_argument("--blue", type=int, metavar="INTEGER", default=0, help="blue level")
+    parser.add_argument("--brightness", type=int, metavar="INTEGER", default=255, help="initial and default brightness")
     parser.add_argument("--mqttbroker", help="mqtt broker")
     parser.add_argument("--id", default="busylight-hass", help="tag for mqtt broker and topics")
     parser.add_argument("--mqttuser", help="mqtt user")
@@ -74,25 +75,88 @@ def get_password(password: str, password_file: str) -> str:
 Outgoing = collections.namedtuple('Outgoing', ['topic', 'payload'])
 
 
-def queue_current_state(light: busylight_core.Light, outgoing: asyncio.Queue, discovery: dict) -> None:
-    outgoing.put_nowait(Outgoing(topic=discovery['state_topic'], payload=discovery['payload_on'] if light.is_lit else discovery['payload_off']))
-    outgoing.put_nowait(Outgoing(discovery['rgb_state_topic'], ",".join([str(c) for c in light.color])))
+def get_int(current: int, fields: list[int], position: int) -> int:
+    if len(fields) < position:
+        return current
+    try:
+        return int(fields[position])
+    except ValueError:
+        return current
 
 
-async def listener(client: aiomqtt.Client, light: busylight_core.Light, discovery: dict, outgoing: asyncio.Queue, colour: tuple[int, int, int]) -> None:
+class Colour:
+    def __init__(self, red: int, green: int, blue:int, brightness: int):
+        self.red = red
+        self.green = green
+        self.blue = blue
+        self.brightness = brightness
+
+    def get_rgb(self) -> tuple[int, int, int]:
+        if  self.brightness >= 255:
+            r = [self.red, self.green, self.blue]
+        elif self.brightness <= 0:
+            r = [0, 0, 0]
+        else:
+            scale = self.brightness / 255.0
+            r = [int(self.red * scale), int(self.green * scale), int(self.blue * scale)]
+        return tuple(r)
+
+    def update(self, fields: list[str], offset: int) -> None:
+        self.red = get_int(self.red, fields, offset)
+        self.green = get_int(self.green, fields, offset + 1)
+        self.blue = get_int(self.blue, fields, offset + 2)
+        self.brightness = get_int(self.brightness, fields, offset + 3)
+
+    def state(self) -> str:
+        return f"{self.red},{self.green},{self.blue},{self.brightness}"
+
+
+def queue_current_state(on: bool, colour: Colour, outgoing: asyncio.Queue, discovery: dict) -> None:
+    payload = f"{"on" if on else "off"},{colour.state()}"
+    outgoing.put_nowait(Outgoing(topic=discovery['state_topic'], payload=payload))
+
+
+async def transition(light: busylight_core.Light, fields: list[str], offset: int, rgb: tuple[int, int, int]) -> None:
+    if light.nleds > 0 and len(fields) > offset and fields[offset]:
+        try:
+            transition = float(fields[offset])
+        except ValueError:
+            logging.info("bad transition from client: %s", fields)
+        else:
+            delay = transition / (light.nleds - 1.0)
+            logging.debug("transition to %s leds=%d %s %f %f %d", rgb, light.nleds, fields, transition, delay, offset)
+            first = True
+            for led in range(1, light.nleds + 1):
+                if not first:
+                    await asyncio.sleep(delay)
+                else:
+                    first = False
+                light.on(color=rgb, led=led)
+            return
+    logging.debug("set %s leds=%d %s %d", rgb, light.nleds, fields, offset)
+    light.on(color=rgb)
+
+
+async def listener(client: aiomqtt.Client, light: busylight_core.Light, discovery: dict, outgoing: asyncio.Queue, colour: Colour, on: bool) -> None:
     async for message in client.messages:
         logging.debug("got a message from client.messages: %s %s", message.topic, message.payload)
         topic = str(message.topic)
         payload = message.payload.decode().lower()
         if topic == discovery['command_topic']:
-            if payload == discovery['payload_off']:
-                light.off()
-                logging.debug("off payload from client: %s %s", message.topic, message.payload)
-            elif payload == discovery['payload_on']:
-                light.on(colour)
-                logging.debug("on payload from client: %s %s %s", message.topic, message.payload, colour)
+            fields = payload.split(",")
+            if fields[0] == "off":
+                await transition(light, fields, offset=1, rgb=(0, 0, 0))
+                on = False
+                logging.debug("after off colours are %s", light.color)
+            elif fields[0] == "on":
+                on = True
+                colour.update(fields, 1)
+                rgb = colour.get_rgb()
+                logging.debug("setting colours to %s", rgb)
+                await transition(light, fields, offset=5, rgb=rgb)
+                logging.debug("after setting colours to %s colours are %s", rgb, light.color)
             else:
-                logging.info("unexpected message from clients: %s %s", message.topic, payload)
+                logging.info("bad message from client: %s %s", message.topic, payload)
         elif topic == discovery['rgb_command_topic']:
             try:
                 components = [int(component) for component in payload.split(',')]
@@ -107,7 +171,7 @@ async def listener(client: aiomqtt.Client, light: busylight_core.Light, discover
         else:
             logging.info("message on unexpected topic from client: %s %s", message.topic, message.payload)
 
-        queue_current_state(light=light, outgoing=outgoing, discovery=discovery)
+        queue_current_state(on=on, colour=colour, outgoing=outgoing, discovery=discovery)
 
     logging.error("end of listener")
 
@@ -115,7 +179,7 @@ async def listener(client: aiomqtt.Client, light: busylight_core.Light, discover
 async def publisher(client: aiomqtt.Client, outgoing: asyncio.Queue) -> None:
     while True:
         message = await outgoing.get()
-        logging.debug("retrieved a message %s to send %d remaining", message, outgoing.qsize())
+        logging.debug("sending %s (%d remaining)", message, outgoing.qsize())
         await client.publish(topic=message.topic, payload=message.payload, retain=True)
         outgoing.task_done()
     logging.error("end of publisher")
@@ -146,13 +210,17 @@ def make_discovery(hardware: busylight_core.Hardware) -> dict:
         },
         "unique_id": identifier,
         "object_id": identifier,
+        "schema": "template",
         "availability_topic": make_topic(hardware, 'availability'),
-        "command_topic": make_topic(hardware, 'command'),
-        "payload_on": "on",
-        "payload_off": "off",
         "state_topic": make_topic(hardware, 'state'),
-        "rgb_command_topic": make_topic(hardware, 'rgb_command'),
-        "rgb_state_topic": make_topic(hardware, 'rgb_state'),
+        "command_topic": make_topic(hardware, 'command'),
+        "command_on_template": "on,{{ red | d }},{{ green | d }},{{ blue  | d }},{{ brightness | d }},{{ transition | d }}",
+        "command_off_template": "off,{{ transition | d }}",
+        "state_template": "{{ value.split(',')[0] }}",  # must return `on` or `off`
+        "brightness_template": "{{ value.split(',')[4] }}",
+        "red_template": "{{ value.split(',')[1] }}",
+        "green_template": "{{ value.split(',')[2] }}",
+        "blue_template": "{{ value.split(',')[3] }}",
         "qos": 2,
     }
     return payload
@@ -171,8 +239,8 @@ async def send_mqtt_configuration(client: aiomqtt.Client, hardware: busylight_co
 async def mqtt(light: busylight_core.Light,
         broker: str, user:str, password:str, clientid:str,
         reconnect_delay: float,
-        colour: tuple[int, int, int],
-        initially_on: bool,
+        colour: Colour,
+        on: bool,
     ) -> None:
     logging.info("aiomqtt.Client(hostname=%s, username=%s, password=password, identifier=%s)", broker, user, clientid)
     discovery = make_discovery(light.hardware)
@@ -184,17 +252,15 @@ async def mqtt(light: busylight_core.Light,
         try: # https://aiomqtt.bo3hm.com/subscribing-to-a-topic.html
             async with client:
                 await client.subscribe(discovery['command_topic'])
-                await client.subscribe(discovery['rgb_command_topic'])
                 await send_mqtt_configuration(client, light.hardware, discovery)
                 if first:
                     first = False
-                    if initially_on:
-                        light.on(color=colour)
-                    else:
-                        light.off()
-                    queue_current_state(light=light, outgoing=outgoing, discovery=discovery)
+                    rgb = colour.get_rgb() if on else (0, 0, 0)
+                    light.on(color=rgb)
+                    logging.debug("initial colours %s read back as %s", rgb, light.color)
+                    queue_current_state(on=on, colour=colour, outgoing=outgoing, discovery=discovery)
                 async with asyncio.TaskGroup() as tg:
-                    tg.create_task(listener(client=client, light=light, discovery=discovery, outgoing=outgoing, colour=colour))
+                    tg.create_task(listener(client=client, light=light, discovery=discovery, outgoing=outgoing, colour=colour, on=on))
                     tg.create_task(publisher(client=client, outgoing=outgoing))
             logging.error("after async")
         except aiomqtt.MqttError:
@@ -209,11 +275,12 @@ def make_mqtt_clientid(hostname: str, device: str) -> str:
     return f"{new_hostname}busylighthass"[0:23 - len(new_device)] + new_device
 
 
-async def flash_light(light: busylight_core.Light, colour: tuple[int, int, int]):
-    for led in [1, 3, 5, 7]:
-        light.on(color=colour, led=led)
+async def flash_light(light: busylight_core.Light, colour: Colour):
+    colours = colour.get_rgb()
+    for led in range(1, light.nleds + 1):
+        light.on(color=colours, led=led)
         await asyncio.sleep(0.05)
-    logging.debug("flashed %s colour=%s", light, colour)
+    logging.debug("flashed %s colour=%s", light, colours)
 
 
 def get_light(path: str) -> busylight_core.Light:
@@ -234,7 +301,7 @@ async def main():
     logging.basicConfig(level=numeric_level)
     
     device = options.path[0]
-    colour = (options.red, options.green, options.blue)
+    colour = Colour(red=options.red, green=options.green, blue=options.blue, brightness=options.brightness)
 
     light = get_light(path=device)
     if options.initially_on:
@@ -251,7 +318,7 @@ async def main():
                 clientid=make_mqtt_clientid(hostname=hostname, device=device),
                 reconnect_delay=options.reconnect,
                 colour=colour,
-                initially_on=options.initially_on,
+                on=options.initially_on,
             ))
 
 
